42d4270554d520f60db2ab45301d59ebee793c8e
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-02-23 18:59:50 +0100

    Added fee to bankAccountWithFee

--- basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithFee.java
+++ basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithFee.java
@@ -3,8 +3,10 @@ package example.model;
 public class SimpleBankAccountWithFee implements BankAccount {
 
     private final SimpleBankAccount bankAccount;
-    public SimpleBankAccountWithFee(AccountHolder ah, double balance){
+    private final double fee;
+    public SimpleBankAccountWithFee(AccountHolder ah, double balance, double fee){
         bankAccount = new SimpleBankAccount(ah, balance);
+        this.fee = fee;
     }
     @Override
     public double getBalance() {
@@ -13,11 +15,12 @@ public class SimpleBankAccountWithFee implements BankAccount {
 
     @Override
     public void deposit(int userID, double amount) {
-
+        bankAccount.deposit(userID, amount);
     }
 
     @Override
     public void withdraw(int userID, double amount) {
-
+        bankAccount.withdraw(userID, amount);
+        bankAccount.withdraw(userID, fee);
     }
 }
--- basic-refactoring-exercise/src/test/java/SimpleBankAccountWithFeeTest.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountWithFeeTest.java
@@ -9,17 +9,27 @@ public class SimpleBankAccountWithFeeTest {
 
     private AccountHolder accountHolder;
     private SimpleBankAccountWithFee bankAccountWithFee;
+    private final int ACCOUNT_ID = 1;
     private final double INITIAL_BALANCE = 0;
+    private final double FEE_AMOUNT = 1;
     private final double INITIAL_DEPOSIT = 100;
 
     @BeforeEach
     void init(){
-        accountHolder = new AccountHolder("Mario", "Rossi", 1);
-        bankAccountWithFee = new SimpleBankAccountWithFee(accountHolder, INITIAL_BALANCE);
+        accountHolder = new AccountHolder("Mario", "Rossi", ACCOUNT_ID);
+        bankAccountWithFee = new SimpleBankAccountWithFee(accountHolder, INITIAL_BALANCE, FEE_AMOUNT);
     }
     @Test
     void testInitialBalance() {
         assertEquals(INITIAL_BALANCE, bankAccountWithFee.getBalance());
     }
+    @Test
+    void testBalanceAfterWithdrawalWithFee(){
+        final double WITHDRAWAL_AMOUNT = 10;
+        bankAccountWithFee.deposit(ACCOUNT_ID, INITIAL_DEPOSIT);
+        bankAccountWithFee.withdraw(ACCOUNT_ID, WITHDRAWAL_AMOUNT);
+        assertEquals(INITIAL_DEPOSIT - WITHDRAWAL_AMOUNT - FEE_AMOUNT, bankAccountWithFee.getBalance());
+    }
+
 
 }
