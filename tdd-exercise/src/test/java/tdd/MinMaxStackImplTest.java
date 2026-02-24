432659e1c6697c004ed1b33b9af886248684d222
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-02-24 12:17:35 +0100

    Stack returns true when empty

--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -6,7 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void stackShouldInitiallyBeEmpty(){
+        MinMaxStack stack = new MinMaxStackImpl();
+        assertTrue(stack.isEmpty());
     }
 }
